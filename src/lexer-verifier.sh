84e81132a015e91b861f1f8cee657524be4ae395
Gabriel Ziegler <gabrielziegler3@gmail.com>
2018-05-25 19:00:56 -0300

    Add script for Lexico.md verification
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>

---
+++ src/lexer-verifier.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+read -p "Enter Lexico.md dir: " dir
+
+file="Léxico.md"
+
+CYAN='\033[0;36m'
+RED='\033[0;31m'
+
+lexer_files=$(grep -E -o "\[(.+?)\]" $dir$file | sed 's/\[\|\]//g' | sed 's/$/.md/g' | sed 's/ /-/g')
+
+echo -e "Checking Lexicos...\n"
+
+sleep .7
+
+for i in "$lexer_files"
+do
+	echo -e "1- Verifying Pattern\n"
+	echo -e "\n\n\n"
+
+	grep --color "Sinônimos" -A 1 $dir$i
+	read -p "Press enter to continue"
+
+	echo -e "\n\n\n"
+	grep --color "Noção" -A 1 $dir$i
+	read -p "Press enter to continue"
+
+	echo -e "\n\n\n"
+	grep --color "Impacto" -A 1 $dir$i
+	read -p "Press enter to continue"
+done
+
